b3ba1949832eea55279ba7f2f2258c75a90fe616
L1 <pubgtjes@gmail.com>
2023-03-13 11:28:34 +0100

    feat(backend) documented permission middleware

--- app/Http/Middleware/PermissionMiddleware.php
+++ app/Http/Middleware/PermissionMiddleware.php
@@ -15,38 +15,57 @@ class PermissionMiddleware
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param string  $permissions the permissions that are required, this is a string with the permissions separated by &
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle(Request $request, Closure $next, string $permissions): Response
     {
-        $allowed = true;
-        $UserId = $request->user()?->id;
-
+        $Allowed = true;
         $PermissionArray = explode('&', $permissions);
 
+        $UserId = $request->user()?->id;
+        $UserModel = User::where('id', $UserId) -> first();
+        $UserPermissions = $UserModel -> permissions;
 
+        // Convert the permission names to ids
         $PermissionArray = array_map(function ($permission) {
             return $this->PermissionToId($permission);
         }, $PermissionArray);
 
-        $UserModel = User::where('id', $UserId) -> first();
-        $UserPermissions = $UserModel -> permissions;
 
+        // Loop through the required permissions and check if the user has them, if the user doesn't have one of the permissions, set $Allowed to false
         foreach ($PermissionArray as $PermissionArrayItem) {
             if (!$UserPermissions->contains($PermissionArrayItem)) {
-                $allowed = false;
+                $Allowed = false;
             }
         }
 
-        if ($allowed) {
+        // If the user has all the permissions, return the next middleware
+        if ($Allowed) {
             return $next($request);
-        } else {
-            abort(403);
         }
 
+        // User didn't pass the checks, return a 403 response
+        abort(403);
     }
 
-    private function PermissionToId($permission): int {
+    /**
+     * Get the id of a permission name with cache
+     * @param string $permission the name of the permission
+     * @return int the id of the permission
+     */
+    private function PermissionToId(String $permission): int {
+        $CachePrefix = 'permissions_';
+        $CacheItem = \Cache::get($CachePrefix .$permission);
+
+        if ($CacheItem) {
+            return $CacheItem;
+        }
+
         $PermissionModel = Permission::where('name', $permission) -> firstOrFail();
+
+        \Cache::put($CachePrefix .$permission, $PermissionModel -> id, 60 * 5);
+
         return $PermissionModel -> id;
     }
 }
