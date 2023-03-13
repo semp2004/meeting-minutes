8bd7c94209bbac8bce5acdcaed567202fd0ec07c
L1 <pubgtjes@gmail.com>
2023-03-13 11:27:54 +0100

    feat(frontend) welcome blade + template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,10 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Tailwind CDN -->
+        <script src="https://cdn.tailwindcss.com"></script>
+
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
@@ -19,17 +23,13 @@
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+            <div class="text-gray-100 text-center text-xl">
+                @yield('header')
+            </div>
 
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
+            <main class="text-gray-100">
+                @yield('content')
             </main>
         </div>
     </body>
