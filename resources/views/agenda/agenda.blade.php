1bdeb8bf1c684dca74c551985336cf601fa40bca
Sem Peters <00303712@rijnijssel.nl>
2023-03-16 11:18:46 +0100

    added a basic laout for meeting agenda, might change the /agenda

---
+++ resources/views/agenda/agenda.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('content')
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Users') }}
+        </h2>
+    </x-slot>
+    {{--naam meeting, datum meeting, naam van eigenaar van meeting--}}
+    {{--knop aanpassen, nieuwe meeting knop--}}
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="overflow-hidden overflow-x-auto p-6 bg-gray-800">
+                    <div class="min-w-full align-middle">
+                        <table class="min-w-full border border-gray-600 divide-y divide-gray-600">
+                            <thead>
+                            <tr>
+                                <th class="bg-gray-800 px-6 py-3 bg-gray-700 text-left">
+                                    <span class="text-xs leading-4 font-medium text-white uppercase tracking-wider">Naam van meeting</span>
+                                </th>
+                                <th class="bg-gray-800 px-6 py-3 bg-gray-700 text-left border-l border-l-gray-700">
+                                    <span class="text-xs leading-4 font-medium text-white uppercase tracking-wider">datum meeting</span>
+                                </th>
+                                <th class="bg-gray-800 px-6 py-3 bg-gray-700 text-left border-l border-l-gray-700">
+                                    <span class="text-xs leading-4 font-medium text-white uppercase tracking-wider">Naam eigenaar</span>
+                                </th>
+                                <th class="bg-gray-800 px-6 py-3 bg-gray-700 text-left border-l border-l-gray-700">
+                                    <span class="text-xs leading-4 font-medium text-white uppercase tracking-wider">Meeting aanpassen</span>
+                                </th>
+                            </tr>
+                            </thead>
+
+                            <tbody class="bg-gray-800 divide-y divide-gray-700">
+
+                            <tr class="bg-gray-800">
+                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-white">
+                                    [naam]
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-white border-l border-l-gray-700">
+                                    [dd-mm-yyyy]
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-white border-l border-l-gray-700">
+                                    [naam]
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-white border-l border-l-gray-700">
+                                    [knop]
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div class="mt-2">
+                        [knop om meeting toe te voegen]
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
