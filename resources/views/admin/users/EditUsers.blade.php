8c17a1bfc53aa2cc795444596026c5b79283dde2
Čapčuch, Patrik - Student Rijn IJssel <00317034@rijnijssel.nl>
2023-03-21 10:52:11 +0100

    added background and some padding on h1 of EditUsers page

--- resources/views/admin/users/EditUsers.blade.php
+++ resources/views/admin/users/EditUsers.blade.php
@@ -3,36 +3,38 @@
 
 
 @section('header')
-    <h1>Gebruikers aanpassen</h1>
+    <h1 class="mt-4 mb-6">Gebruikers aanpassen</h1>
 @endsection
 
 
 
 @section('content')
-    <table class="text-center text-gray-100 w-full">
-        <tr>
-            <th>Naam</th>
-            <th>Email</th>
-            <th>Acties</th>
-
-
-        @foreach($users as $user)
+    <div class="bg-gray-800 rounded pt-6 pb-8 mb-4 ml-20 mr-20">
+        <table class="text-center text-gray-100 w-full">
             <tr>
-                <td>{{$user->name}}</td>
-                <td>{{$user->email}}</td>
-
-                <td class="grid grid-cols-2">
-                    <form method="get" action="{{route('EditUser', ['id' => $user -> id])}}">
-                        <x-secondary-button type="submit">Aanpassen</x-secondary-button>
-                    </form>
-
-                    <form method="get" action="{{route('DeleteUser', ['id' => $user -> id])}}">
-                        <x-secondary-button type="submit">Verwijderen</x-secondary-button>
-                    </form>
-
-                </td>
+                <th>Naam</th>
+                <th>Email</th>
+                <th>Acties</th>
             </tr>
-        @endforeach
 
-    </table>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+
+                    <td class="grid grid-cols-2">
+                        <form method="get" action="{{route('EditUser', ['id' => $user -> id])}}">
+                            <x-secondary-button type="submit">Aanpassen</x-secondary-button>
+                        </form>
+
+                        <form method="get" action="{{route('DeleteUser', ['id' => $user -> id])}}">
+                            <x-secondary-button type="submit">Verwijderen</x-secondary-button>
+                        </form>
+
+                    </td>
+                </tr>
+            @endforeach
+
+        </table>
+    </div>
 @endsection
