e25949688f49079dd441a4c46b374672a4a3653e
thijmen <00318236@rijnijssel.nl>
2023-03-21 08:26:31 +0100

    feat(migrations) gave user 11 all the permissions.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        (new UserSeeder())->run();
         (new PermissionSeeder())->run();
+        (new UserSeeder())->run();
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Permission;
+use App\Models\UserPermission;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +16,17 @@ class UserSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
+        $ExampleUser = \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        for ($i = 1; $i < count(\App\Enums\Permission::cases()); $i++) {
+            UserPermission::query()
+                ->create([
+                    'user_id' => $ExampleUser -> id,
+                    'permission_id' => $i,
+                ]);
+        }
     }
 }
