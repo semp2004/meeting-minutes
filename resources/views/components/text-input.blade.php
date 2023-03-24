a4a1012d8a2c57c9dca61807ff65fe82a6692ff1
Čapčuch, Patrik - Student Rijn IJssel <00317034@rijnijssel.nl>
2023-03-24 09:55:23 +0100

    text-input fix.

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false, 'value' => '', 'name' => ''])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'dark:bg-gray-800 bg-gray-200 dark:text-gray-200 text-gray-800 border-gray-700 border-[1px] px-4 py-2 rounded-lg w-full focus:outline-none focus:border-gray-400'])!!} name="{{$name}}" value="{{$value}}">
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'dark:bg-gray-800 bg-gray-200 dark:text-gray-200 text-gray-800 border-gray-700 border-[1px] px-4 py-2 rounded-lg w-full focus:outline-none focus:border-gray-400'])!!} name="{{$name}}" value="{{ html_entity_decode($value, ENT_QUOTES) }}">
