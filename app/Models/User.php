3333e7dacb2d659b2e4705d3e7e73a0e70add9b3
Sem Peters <00303712@rijnijssel.nl>
2023-03-28 11:26:58 +0200

    removed double meetings function

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,10 +55,6 @@ class User extends Authenticatable
         return $this->hasMany(Meeting::class);
     }
 
-    public function meetings(): HasMany
-    {
-        return $this->hasMany(Meeting::class);
-    }
 }
 
 ///**
