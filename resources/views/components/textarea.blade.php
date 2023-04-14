99d0c27e4b32f25d1f0b98162856f2906896eba8
Čapčuch, Patrik - Student Rijn IJssel <00317034@rijnijssel.nl>
2023-04-14 12:02:08 +0200

    fix(frontend) removed whitespaces.

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -4,6 +4,4 @@
     $text = html_entity_decode($value, ENT_QUOTES);
 @endphp
 
-<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'dark:bg-gray-800 bg-gray-200 dark:text-gray-200 text-gray-800 border-gray-700 border-[1px] px-4 py-2 rounded-lg w-full focus:outline-none focus:border-gray-400'])!!} name="{{$name}}">
-    {{$text}}
-</textarea>
+<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'dark:bg-gray-800 bg-gray-200 dark:text-gray-200 text-gray-800 border-gray-700 border-[1px] px-4 py-2 rounded-lg w-full focus:outline-none focus:border-gray-400'])!!} name="{{$name}}">{{$text}}</textarea>
